3093bce3cccfeeda2db80c9f728cbe30ac146082
mrjadaml <mrjadaml@gmail.com>
2016-12-12 10:07:24 -0800

    point mocha to node modules + cleanup 🚿

--- test/hash-tables/test.js
+++ test/hash-tables/test.js
@@ -10,18 +10,16 @@ describe('Hash Table', () => {
       greet('english').should.equal('Welcome a Miami.');
     });
 
-    it('should greet user in Dutch if the provided lang is "dutch"', () => {
+    xit('should greet user in Dutch if the provided lang is "dutch"', () => {
       greet('dutch').should.equal('Welkom a Miami.');
     });
 
-    it('should greet the user in English for invalid inputs', () => {
+    xit('should greet the user in English for invalid inputs', () => {
       greet('bob').should.equal('Welcome a Miami.');
+    });
+
+    xit('should greet the user with the correct lang even if it the input is capitolized', () => {
+      greet('Swedish').should.equal('Valkommen a Miami.');
     });
   });
 });
-
-
-
-//Test.assertEquals(greet('english'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
-//Test.assertEquals(greet('dutch'), 'Welkom', "Your function should have returned 'Welkom'. Try again.");
-//Test.assertEquals(greet('IP_ADDRESS_INVALID'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
