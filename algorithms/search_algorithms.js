7ae411dfd88f9335edcf8bd1a2414f9d77052c6d
rrathbone <rachellerathbone@gmail.com>
2016-12-12 15:53:58 -0800

    complete linear and binary search

--- algorithms/search_algorithms.js
+++ algorithms/search_algorithms.js
@@ -1,11 +1,33 @@
 'use strict'
 
 const linearSearch = function(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
 
+  return -1;
 }
 
 const binarySearch = function(array, value) {
+  let low  = 0,
+      high = array.length - 1,
+      mid;
 
+  while (low <= high) {
+    mid = Math.floor(low + (high - low) / 2);
+
+    if (array[mid] === value) {
+      return mid;
+    } else if (array[mid] < value) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+
+  }
+  return - 1;
 }
 
 module.exports = { linearSearch, binarySearch };
