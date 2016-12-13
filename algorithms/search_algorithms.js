7769b273b0a55c8a070c3281d80db25ef8afa899
Ken McGrady <ken.mcgrady@gmail.com>
2016-12-13 08:04:58 -0800

    Add Sort

--- algorithms/search_algorithms.js
+++ algorithms/search_algorithms.js
@@ -1,7 +1,13 @@
 'use strict'
 
 const linearSearch = function(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
 
+  return -1;
 }
 
 const binarySearch = function(array, value) {
