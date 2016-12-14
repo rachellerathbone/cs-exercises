efc040a5e0dda6fbece7992d2bfe9dca72773059
Ken McGrady <ken.mcgrady@gmail.com>
2016-12-14 15:42:14 -0800

    Update Linked Lists

---
+++ linked_lists/level2.js
@@ -0,0 +1,36 @@
+// Write a function named insertInBack that takes in the following:
+//   value (a number)
+//   list (a linked list)
+// The function returns The linked list with the node inserted at the end.
+// Example:
+//    insertInBack(1 -> 2 -> 3 -> ., 4) produces 1 -> 2 -> 3 -> 4 -> .
+function insertInBack(value, list) {
+
+}
+
+// Write a function named removeNodeAtIndex which takes in the following:
+//   list (a linked list)
+//   index (a number)
+// The function will update the linked list removing the item at that index.
+// The function will not make any changes if the index is invalid.
+// Example:
+//   removeNodeAtIndex(1 -> 2 -> 3 -> ., 1) produces 1 -> 3 -> .
+//   removeNodeAtIndex(1 -> 2 -> 3 -> ., 2) produces 1 -> 2 -> .
+function removeNodeAtIndex(list, index) {
+
+}
+
+// Write a function named reverse that takes in the following:
+//   list (a linked list)
+// The function returns a NEW linked list with the items in reverse order.
+// Example:
+//   1 -> 2 -> 3 -> . would produce 3 -> 2 -> 1 -> .
+function reverse(list) {
+
+}
+
+module.exports = {
+  insertInBack,
+  removeNodeAtIndex,
+  reverse
+}
